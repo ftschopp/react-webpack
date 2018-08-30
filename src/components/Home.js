098718b00c4ab2b1a8cf2a5370dd846a238f513f
Fernando Tschopp <tschoppfernando@gmail.com>
2018-08-30 08:55:40 -0300

    commit inicial

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Grid } from 'semantic-ui-react';
+
+import Layout from './Layout';
+//import Features from './Features';
+//import Profile from './Profile';
+
+const Home = () => {
+  return (
+   <div>hola home</div>
+  );
+};
+
+export default Home;
